b632d78bb09e2ba3904191973a896dd93966fdaf
Erin Kreis <erin@silverstrides.com>
2024-03-29 23:36:37 -0400

    Made header responsive

--- public/app.js
+++ public/app.js
@@ -110,3 +110,26 @@ document.addEventListener("DOMContentLoaded", function () {
   addCheckboxListener("vitaminD");
   addCheckboxListener("magnesium");
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Hamburger menu
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector(".nav-menu");
+
+  hamburger.addEventListener("click", mobileMenu);
+
+  function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  }
+
+  // Close hamburger menu when a link is clicked
+  const navLink = document.querySelectorAll(".nav-link");
+
+  navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+  function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  }
+});
